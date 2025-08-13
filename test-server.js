5a08285e1f8f4aa09c45dfdf5193e2681c703656
Tim Sinclair <Tim.Sinclair@live.com>
2025-08-12 19:33:25 -0700

    Ultra-simple test server

---
+++ test-server.js
@@ -0,0 +1,13 @@
+﻿console.log('Starting server...');
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  console.log('Request received');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World - Server is working!');
+});
+
+server.listen(PORT, () => {
+  console.log('Server listening on port', PORT);
+});
